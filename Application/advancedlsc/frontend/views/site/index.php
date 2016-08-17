72a3a4c3d1fcf713183955aa52f7069050b73fbd
Danya Balcena <dpbalcena@gmail.com>
2016-08-17 22:09:23 +0800

    added codes for index.php

--- Application/advancedlsc/frontend/views/site/index.php
+++ Application/advancedlsc/frontend/views/site/index.php
@@ -1,8 +1,103 @@
-<!DOCTYPE html>
-<html>
-<head>
-</head>
+<?php
 
-</body>
-</html>
+/* @var $this yii\web\View */
 
+$this->title = 'Home';
+?>
+
+<style>
+
+</style>
+
+<div class="site-index">
+
+<!--start of slideshow-->
+<div class="slideshow-container">
+
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="http://www.w3schools.com/howto/img_nature_wide.jpg" style="width:100%">
+  <div class="text">Promo 1</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="http://www.w3schools.com/howto/img_fjords_wide.jpg" style="width:100%">
+  <div class="text">Promo 2</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="http://www.w3schools.com/howto/img_lights_wide.jpg" style="width:100%">
+  <div class="text">Promo 3</div>
+</div>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot"></span>
+  <span class="dot"></span>
+  <span class="dot"></span>
+</div>
+
+<script>
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 4000); // Change image every 9 seconds
+}
+</script>
+<!--end of slideshow-->
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>About</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://localhost/advanced/frontend/web/index.php?r=site%2Fabout">Read more &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Tutorials</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://localhost/advanced/frontend/web/index.php?r=site%2Ftutorials">Read more &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Reviews</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://localhost/advanced/frontend/web/index.php?r=site%2Freviews">Read more &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
