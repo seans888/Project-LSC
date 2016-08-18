42b80e6365dab58bf8c36611c29c3c500878cf43
Danya Balcena <dpbalcena@gmail.com>
2016-08-18 13:51:33 +0800

    Minor changes
    Title of tabs

--- Application/advancedlsc/frontend/views/site/about.php
+++ Application/advancedlsc/frontend/views/site/about.php
@@ -3,15 +3,15 @@
 /* @var $this yii\web\View */
 
 use yii\helpers\Html;
-/*
 $this->title = 'About Us';
+/*
+
 $this->params['breadcrumbs'][] = $this->title;
 */
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
-	
-<h1> About Us </h1>	
+
 <hr>
 <!--start of about-->
 
