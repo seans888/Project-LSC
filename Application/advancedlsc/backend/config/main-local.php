44a7d5a8fb94839699237a40bab0b6b79992538c
Danya Balcena <dpbalcena@gmail.com>
2016-08-18 13:37:43 +0800

    main-local.php

--- Application/advancedlsc/backend/config/main-local.php
+++ Application/advancedlsc/backend/config/main-local.php
@@ -4,7 +4,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '_zCLfTdeMRzJXRmF3-7vTU--jFmSD5TI',
+            'cookieValidationKey' => 'KdDb7vX_DjmZZYAxSuWQ1vChNI3PYlz2',
         ],
     ],
 ];
