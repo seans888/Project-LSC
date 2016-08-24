546eac9b4cb4d9fe9b3beeef590cda82c188618e
Danya Balcena <dpbalcena@gmail.com>
2016-08-24 13:37:02 +0800

    backend/views/class-list/index.php

--- Application/advancedlsc - Copy/backend/views/class-list/index.php
+++ Application/advancedlsc - Copy/backend/views/class-list/index.php
@@ -12,7 +12,7 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="class-list-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+  <!--  <h1><?= Html::encode($this->title) ?></h1> -->
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
@@ -22,7 +22,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+           // ['class' => 'yii\grid\SerialColumn'],
 
             'id',
             'student_id',
