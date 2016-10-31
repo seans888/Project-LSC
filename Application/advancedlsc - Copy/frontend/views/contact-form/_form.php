74630f542702d732d601d045f89ddf7cb6949857
Danya Balcena <dpbalcena@gmail.com>
2016-10-31 17:37:49 +0800

    added popup message when logout button is clicked

--- Application/advancedlsc - Copy/frontend/views/contact-form/_form.php
+++ Application/advancedlsc - Copy/frontend/views/contact-form/_form.php
@@ -21,7 +21,6 @@ use yii\widgets\ActiveForm;
                          </p>
                  </div>
              </div>
-             <!--/.HEADER LINE END-->
            <div class="row set-row-pad"  data-scroll-reveal="enter from the bottom after 0.5s" >
                  <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2">
                    
@@ -34,7 +33,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'content')->label(false)->textarea(['rows' => 6,'placeholder'=>'Message']) ?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Submit Message' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success1' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? 'SUBMIT MESSAGE' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success1' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
