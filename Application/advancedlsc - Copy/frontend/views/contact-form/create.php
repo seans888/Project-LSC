b7b7c9da3e531c13b2723274cddb61d43aabc128
Danya Balcena <dpbalcena@gmail.com>
2016-10-31 15:08:28 +0800

    Improved design of contact-form page and installed kartik dialog

--- Application/advancedlsc - Copy/frontend/views/contact-form/create.php
+++ Application/advancedlsc - Copy/frontend/views/contact-form/create.php
@@ -12,7 +12,6 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="contact-form-create">
 
-    <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,
