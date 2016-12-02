ca335e4ea5714e477bbafbe040878366c6f5826c
Gail Haboc <fghaboc@gmail.com>
2016-12-02 08:55:58 +0800

    updated Quality Plan Document and backend forms

--- Application/loyolastudentcenter2/backend/views/transaction/_form.php
+++ Application/loyolastudentcenter2/backend/views/transaction/_form.php
@@ -14,17 +14,15 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'user_id')->textInput() ?>
 
-    <?= $form->field($model, 'review_class_id')->textInput() ?>
+    <?= $form->field($model, 'selected_school')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'schedule_id')->textInput() ?>
+    <?= $form->field($model, 'review_class_id')->textInput() ?>
 
     <?= $form->field($model, 'transaction_type')->dropDownList([ 'Reserve' => 'Reserve', 'Enroll' => 'Enroll', ], ['prompt' => '']) ?>
 
-    <?= $form->field($model, 'selected_school')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'schedule_id')->textInput() ?>
 
-    <?= $form->field($model, 'status')->dropDownList([ 'Pending' => 'Pending', 'Reserved' => 'Reserved', 'Enrolled' => 'Enrolled', 'Done' => 'Done', 'Cancelled' => 'Cancelled', ], ['prompt' => '']) ?>
-
-    <?= $form->field($model, 'date')->textInput() ?>
+     <?= $form->field($model, 'status')->dropDownList([ 'Pending' => 'Pending', 'Reserved' => 'Reserved', 'Enrolled' => 'Enrolled', 'Done' => 'Done', 'Cancelled' => 'Cancelled', ], ['value'=>'Pending','prompt' => '']) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
